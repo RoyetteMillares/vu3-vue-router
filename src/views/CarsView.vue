cd6edce6a4b0cc1164227bcc6e19067c33b603de
Royette B. Millares <70820776+RoyetteMillares@users.noreply.github.com>
2023-01-16 20:57:01 +0800

    Url Parameters

---
+++ src/views/CarsView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import cars from '../data/cars.json'
+
+const route = useRoute()
+
+const car = cars.find(c => c.id === parseInt(route.params.id))
+
+</script>
+
+
+<template>
+    <section>
+        <h1>cars view</h1>
+        <p>{{ car }}</p>
+    </section>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import cars from '../data/cars.json'
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+  <main>
+    <h1>Home page</h1>
+
+    <div class="cars-wp">
+      <RouterLink v-for="car in cars" :key="car.id" :to="`/cars/${car.id}`">
+        {{ car.name }}
+      </RouterLink>
+    </div>
+
+  </main>
+</template>
+
+
+<style scoped>
+.cars-wp {
+  display: flex;
+  gap: 2rem;
+}
+
+a {
+  text-decoration: none;
+  padding: 0.75rem;
+  color: #000;
+  width: 100%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.8);
+}
+</style>
